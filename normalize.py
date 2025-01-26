4315415ddd2a0199dd87f355a79bf74b3886e0d5
raulpy271 <raulrufino0@gmail.com>
2025-01-26 10:52:50 -0300

    Ajusta script de normalização de dados

--- normalize.py
+++ normalize.py
@@ -1,15 +1,16 @@
 
-import numpy
 import pandas
 from sklearn.preprocessing import normalize
 
 ignore = set(['PERC_DESVINCULADO'])
-df = pandas.read_csv('processed-dados.csv', sep=';', encoding='latin-1')
 
-for col, series in df.items():
-    if col in ignore:
-        continue
-    df[col] = normalize([series])[0]
+for filename in ['preprocessado', 'preprocessado-privado', 'preprocessado-publico']:
+    df = pandas.read_csv(f'dados/{filename}.csv', sep=';', encoding='latin-1')
 
-print(df.shape)
-df.to_csv('normalized-dados.csv', sep=';', index=False, encoding='latin-1')
+    for col, series in df.items():
+        if col in ignore:
+            continue
+        df[col] = normalize([series])[0]
+
+    print(df.shape)
+    df.to_csv(f'dados/normalizado-{filename}.csv', sep=';', index=False, encoding='latin-1')
