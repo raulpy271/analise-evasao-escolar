67b5690b15b44df53975321f6b55bee7535b1982
raulpy271 <raulrufino0@gmail.com>
2025-02-12 21:02:44 -0300

    Adiciona resultado de treinamento

--- forest_feature_importance.py
+++ forest_feature_importance.py
@@ -8,7 +8,7 @@ from sklearn.tree import plot_tree
 
 from preprocessing import columns
 
-df = pandas.read_csv('normalized-dados.csv', sep=';', encoding='latin-1')
+df = pandas.read_csv('dados/normalizado-preprocessado-publico.csv', sep=';', encoding='latin-1')
 X, y = df[columns], df['PERC_DESVINCULADO']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
@@ -18,17 +18,20 @@ regr = regr.fit(X_train, y_train)
 
 imp = pandas.Series(regr.feature_importances_, index=columns)
 imp.sort_values(inplace=True)
+print(imp[-15:].values)
+print(imp[-15:].values.sum())
+print(imp.values.sum())
 
 fig, ax = plt.subplots(figsize=(12, 8))
-imp[-30:].plot.bar(ax=ax)
+imp[-15:].plot.bar(ax=ax)
 print('Features consideradas: ', imp[imp != 0].count())
-ax.set_title('MDI das 30 features mais importantes - Random Forest')
-ax.set_ylabel("Mean decrease in impurity")
+ax.set_title('MDI das 15 features mais importantes - Universidade pública')
+ax.set_ylabel("Mean Decrease Impurity")
 fig.tight_layout()
-fig.savefig('forest_importance.png')
+fig.savefig('forest_importance_publico.png')
 
-for i in sample(range(len(regr.estimators_)), k=5):
-    fig, ax = plt.subplots(figsize=(20, 20))
-    plot_tree(regr.estimators_[i], feature_names=columns, ax=ax)
-    fig.savefig(f'tree_{i}.png')
+#for i in sample(range(len(regr.estimators_)), k=5):
+#    fig, ax = plt.subplots(figsize=(20, 20))
+#    plot_tree(regr.estimators_[i], feature_names=columns, ax=ax)
+#    fig.savefig(f'tree_{i}.png')
 
