138c987ddfab61f8cce5ea33f6a45c0cbd278dbe
Raul <raulrufino0@gmail.com>
2026-01-02 13:55:57 -0300

    Atualiza tabelas latex com performance dos models

--- performance_table.py
+++ performance_table.py
@@ -2,9 +2,9 @@
 import pandas
 
 metrics = ['mean_absolute_error', 'median_absolute_error', 'mean_squared_error', 'root_mean_squared_error']
-df_geral = pandas.read_csv('train_result.csv', sep=';')
-df_publico = pandas.read_csv('train_result_publico.csv', sep=';')
-df_privado = pandas.read_csv('train_result_privado.csv', sep=';')
+df_geral = pandas.read_csv('resultados/treino-geral.csv', sep=';')
+df_publico = pandas.read_csv('resultados/treino-publico.csv', sep=';')
+df_privado = pandas.read_csv('resultados/treino-privado.csv', sep=';')
 
 col = "Modelo", "Tempo (s)", "MAE", "MSE", "RMSE"
 models = {
@@ -16,7 +16,7 @@ models = {
 data = []
 
 for model, name in models.items():
-    df = df_publico[df_publico["model"] == model]
+    df = df_privado[df_privado["model"] == model]
     time = df["train_time"]
     mae = df["mean_absolute_error"]
     mse = df["mean_squared_error"]
@@ -31,5 +31,5 @@ for model, name in models.items():
 
 df = pandas.DataFrame(data, columns=col)
 
-df.to_latex("performance_table_publico.tex", index=False, decimal=",")
+df.to_latex("tables/performance_table_privado.tex", index=False, decimal=",")
 
